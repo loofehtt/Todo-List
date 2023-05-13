a659b9307cc56af513eb126a9b5abbfc2207dca6
loofehtt <loofehtt@gmail.com>
2023-05-13 18:59:39 +0700

    my todo app

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todolist;
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $todolists = Todolist::all();
+       return view('home', compact('todolists'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'content' => 'required'
+        ]);
+
+        Todolist::create($data);
+        return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todolist $todolist)
+    {
+        $todolist->delete();
+        return back();
+
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List</title>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.css" rel="stylesheet" />
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.js"></script>
+</head>
+
+<body class="bg-info">
+    <div class="container w-25 mt-5">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <h3>Todo List</h3>
+                <form action="{{ route('store') }}" method="POST" autocomplete="off">
+                    @csrf
+                    <div class="input-group">
+                        <input type="text" name='content' class="form-control" placeholder="Add your todo">
+                        <button type="submit" class="btn btn-dark px-4"><i class="fa fa-light fa-plus"></i></button>
+                    </div>
+                </form>
+                {{-- if tasks count --}}
+                @if (count($todolists))
+                    <ul class="">
+                        @foreach ($todolists as $todolist)
+                            <li class="list-group-item">
+                                <form action="{{ route('destroy', $todolist->id) }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="btn btn-link btn-sm float-end"><i
+                                            class="fas fa-trash"></i></button>
+                                </form>
+
+                            </li>
+                        @endforeach
+                    </ul>
+
+                @endif
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
